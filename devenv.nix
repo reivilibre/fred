e997e568c083ee1f204ba252cee7f9dd8695a6c4
Olivier 'reivilibre <contact@librepush.net>
2025-05-28 17:12:53 +0100

    Add build and run dependencies to devenv

--- devenv.nix
+++ devenv.nix
@@ -9,7 +9,29 @@
 
   packages = with pkgs; [
     clang
+    cmake
+    protobuf
+
+    # compilation dependencies (speculated)
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+    xorg.libX11
+    xorg.libxcb
+    libxkbcommon
+
+    alsa-lib
+
+
+    # for runtime?
+    vulkan-loader
   ];
+
+  env = {
+    PROTOC = "${pkgs.protobuf}/bin/protoc";
+
+    LD_LIBRARY_PATH="${pkgs.vulkan-loader}/lib";
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
